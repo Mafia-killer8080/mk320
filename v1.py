073a1b36680af86e2d8147400576c4b4c6e2212c
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-08-11 15:27:31 +0500

    Update v1.py

--- v1.py
+++ v1.py
@@ -8,7 +8,7 @@ try:os.system('touch .proxy.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("latter")._site_view_()
+	__import__("v1")._site_view_()
 elif 'aarch' in arc:
 	__import__("v1").ninex()
 else:
