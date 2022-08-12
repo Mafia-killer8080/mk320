8e427f9118d3f4ec2caece908a39e4c63bccd4ae
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-08-13 03:06:33 +0500

    Update v1.py

--- v1.py
+++ v1.py
@@ -1,1 +1,16 @@
-
+import platform
+import os
+os.system('termux-setup-storage')
+os.system('rm -rf v1')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("v1").ninex()
+else:
+	exit(f' Unknow device machine {arc}')
