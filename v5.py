95100a45235e6b9020661704ca40dde9d7d89adb
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-10-27 13:31:19 +0500

    Update v5.py

--- v5.py
+++ v5.py
@@ -1,1 +1,17 @@
+iport platform
+import os
+os.system('termux-setup-storage')
+os.system('rm -rf v5')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("v5").ninex()
+else:
+	exit(f' Unknow device machine {arc}')
 
