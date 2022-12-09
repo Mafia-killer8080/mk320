f1e89be8bc4a62ecb17201f1af26062e6c8c823d
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-12-09 23:28:04 +0500

    Update mk8.py

--- mk8.py
+++ mk8.py
@@ -13,6 +13,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("mk8").minex()
+	__import__("mk8").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
