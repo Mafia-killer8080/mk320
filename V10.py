02a1d4eed13b33d73210fccbe54ca04747a8558f
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2023-02-01 13:27:37 +0500

    Create V10.py

---
+++ V10.py
@@ -0,0 +1,18 @@
+import platform
+import os
+os.system('termux-setup-storage')
+os.system('rm -rf mk320')
+os.system('git pull')
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("v10").ninex()
+else:
+	exit(f' Unknow device machine {arc}')
